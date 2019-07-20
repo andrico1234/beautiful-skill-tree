8a5cde0654bf5c11003a9d0244d22a9f4f51140a
andrico <andrico1234@yahoo.co.uk>
2019-07-20 10:32:44 +0100

    tree-shake lodash

--- src/context/SkillContext.tsx
+++ src/context/SkillContext.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { mapValues } from 'lodash';
 import { NodeState, ContextStorage } from '../models';
 import { Dictionary } from '../models/utils';
