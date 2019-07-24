d075d7ec5df859ad882ea86340343114752b2be4
andrico <andrico1234@yahoo.co.uk>
2019-07-24 23:25:49 +0100

    remove unused import

--- src/components/__tests__/SkillNode.test.tsx
+++ src/components/__tests__/SkillNode.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SkillNode from '../SkillNode';
 import { NodeState } from 'models';
 import { ThemeProvider } from 'styled-components';
