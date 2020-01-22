1d649c623c52ae01c335b7403cea0ee540500510
andrico <andrico1234@yahoo.co.uk>
2020-01-22 20:45:32 +0000

    check in change

--- example/index.tsx
+++ example/index.tsx
@@ -1,20 +1,20 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import {
-//   SkillTree,
-//   SkillTreeGroup,
-//   SkillProvider,
-//   SkillGroupDataType,
-//   SavedDataType,
-// } from '../src';
 import {
   SkillTree,
   SkillTreeGroup,
   SkillProvider,
   SkillGroupDataType,
   SavedDataType,
-} from '../dist/index';
+} from '../src';
+// import {
+//   SkillTree,
+//   SkillTreeGroup,
+//   SkillProvider,
+//   SkillGroupDataType,
+//   SavedDataType,
+// } from '../dist/index';
 import './index.css';
 import { legsPushData, legsPullData, hpSavedData } from './mockData';
 import { ContextStorage } from '../src/models';
